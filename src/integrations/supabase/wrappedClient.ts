96a6203a307195fda2e31cbad6f867f1f3628ced
Compass25 <compassholidays25@gmail.com>
2025-08-12 08:24:42 +0530

    Update wrappedClient.ts

--- src/integrations/supabase/wrappedClient.ts
+++ src/integrations/supabase/wrappedClient.ts
@@ -1,16 +1,1 @@
-{/*integrations/supabase/wrappedClient.ts
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from './types';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient<Database>(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY,
-  {
-    auth: {
-      detectSessionInUrl: false, // Important to disable auto-login from URL hash tokens
-    },
-  }
-);*/}
